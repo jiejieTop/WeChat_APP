db96fb170968469d8b04812c96b9d0da58c0336f
杰杰 <1161959934@qq.com>
2019-03-30 21:44:12 +0800

    event

---
+++ event/pages/index/index.js
@@ -0,0 +1,32 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    motto: 'Hello World',
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
+  },
+  //事件处理函数
+  bindViewTap: function() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  getUserInfo: function(e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  },
+  tapmessage:function(event){
+    console.log(event);
+  },
+  tapName:function(event) {
+    console.log(event);
+  }
+})
